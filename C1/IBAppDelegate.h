07e6aeba2dea13867d8b52a45bde830df6a870d7
ilyablinov <ilya@Mac-Pro-ilyablinov.local>
2016-09-09 03:45:48 -0700

    v 0.0.1

--- C1/IBAppDelegate.h
+++ C1/IBAppDelegate.h
@@ -5,7 +5,7 @@
 //  Created by ilyablinov on 02.09.16.
 //  Copyright (c) 2016 IB. All rights reserved.
 //
-
+// privet narod v 0/0/1
 #import <UIKit/UIKit.h>
 
 @interface IBAppDelegate : UIResponder <UIApplicationDelegate>
