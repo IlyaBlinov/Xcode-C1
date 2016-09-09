2d84b3cead091741008d19b33b1320531551a09c
ilyablinov <ilya@Mac-Pro-ilyablinov.local>
2016-09-09 03:40:34 -0700

    first commit

--- C1/IBViewController.m
+++ C1/IBViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "IBViewController.h"
-
+#import <ActionSheetStringPicker.h>
 @interface IBViewController ()
+
+@property (strong, nonatomic) ActionSheetStringPicker *ap;
+
 
 @end
 
@@ -18,12 +21,38 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
-}
+    
+    
+    UIBarButtonItem *infoItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemEdit target:self action:@selector(actionEdit:)];
+    
+    self.navigationItem.rightBarButtonItem = infoItem;
+    
+   }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+- (void) sucsessAction:(id) sender{
+    
+}
+
+
+- (void) actionCancell:(id) sender{
+    
+}
+
+- (void) actionEdit: (id) sender{
+    
+    ActionSheetStringPicker *ap = [ActionSheetStringPicker showPickerWithTitle:@"title" rows:@[@"1", @"2", @"3 "] initialSelection:0 target:self successAction:@selector(sucsessAction:) cancelAction:@selector(actionCancell:)  origin: self.navigationItem.rightBarButtonItem];
+    
+    self.ap = ap;
+  
+    
+    
 }
 
 @end
---
+++ Podfile
@@ -0,0 +1,4 @@
+pod 'ActionSheetPicker-3.0'
+pod 'AFNetworking', '~> 2.5.4'
+target 'C1'
+workspace 'C1'
